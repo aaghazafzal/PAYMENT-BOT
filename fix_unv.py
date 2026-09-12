8062441ccf11ddfd3a9fa851ffe6e44eb37aaa3c
aaghazafzal <aghazafzal82@gmail.com>
2026-09-12 16:47:16 +0530

    Updated content or bug fix

---
+++ fix_unv.py
@@ -0,0 +1,12 @@
+﻿import re
+
+with open('C:\\ALL FINAL PROJECTS\\BOTS\\PAYMENT BOT\\src\\api\\routes\\payment.routes.js', 'r', encoding='utf-8') as f:
+    text = f.read()
+
+replacement = "UNV--"
+text = text.replace("ticketId = UNV--;", f"ticketId = {replacement};")
+
+with open('C:\\ALL FINAL PROJECTS\\BOTS\\PAYMENT BOT\\src\\api\\routes\\payment.routes.js', 'w', encoding='utf-8') as f:
+    f.write(text)
+
+print("Fixed UNV-- syntax error")
